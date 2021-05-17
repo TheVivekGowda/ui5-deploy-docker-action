6f4b12d6ede1e248a815f5f678c12db70be4b3e1
Vivek Gowda <TheVivekGowda@gmail.com>
2021-05-17 22:39:11 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 mbt build --mtar -p=neo -t ui5app.mtar
 
-neo.sh deploy-mta --host eu2.hana.ondemand.com --account jrqll30d0f --source /project/ui5app.mtar --synchronous --user S0021327149 --password jgmpp712GWSk
+deploy-mta --host eu2.hana.ondemand.com --account jrqll30d0f --source /project/ui5app.mtar --synchronous --user S0021327149 --password jgmpp712GWSk
