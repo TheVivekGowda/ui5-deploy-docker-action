0e0166b2bf82a788a401a62f5366f855c8b0f685
Vivek Gowda <TheVivekGowda@gmail.com>
2021-05-17 12:31:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
 
-docker run -u root -v "$(pwd):/project" devxci/mbtci:latest mbt build --mtar -p=neo -t ui5app.mtar
+mbt build --mtar -p=neo -t ui5app.mtar
