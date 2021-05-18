c1f5fa5bf30f1ce99e0a17bd0719b8906c2b357e
Vivek Gowda <TheVivekGowda@gmail.com>
2021-05-18 12:53:38 +0530

    Update neo.sh

--- neo.sh
+++ neo.sh
@@ -2,6 +2,6 @@
 
 mbt build --mtar -p=neo -t ui5app.mtar
 
-ls /
+ls -R
 
-/usr/bin/neo.sh deploy-mta --host eu2.hana.ondemand.com --account jrqll30d0f --source /project/ui5app.mtar --synchronous --user S0021327149 --password jgmpp712GWSk
+deploy-mta --host eu2.hana.ondemand.com --account jrqll30d0f --source /project/ui5app.mtar --synchronous --user S0021327149 --password jgmpp712GWSk
