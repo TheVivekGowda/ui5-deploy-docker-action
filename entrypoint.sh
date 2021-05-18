e7eb0ff4a0e54b7bf181101ddba420e3a982b98a
Vivek Gowda <TheVivekGowda@gmail.com>
2021-05-18 11:42:37 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -l
 
-mbt build --mtar -p=neo -t ui5app.mtar
+# mbt build --mtar -p=neo -t ui5app.mtar
+
+docker run -u root -v "$(pwd):/project" devxci/mbtci:latest mbt build --mtar -p=neo -t ui5app.mtar
