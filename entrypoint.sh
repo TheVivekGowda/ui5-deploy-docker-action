351ae79b0753c58a3cf61f7091775346bf68c0aa
Vivek Gowda <TheVivekGowda@gmail.com>
2021-05-20 15:03:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,7 @@ mbt build --mtar -p=neo -t ui5app.mtar
 
 # deploy mtar file to cloud
 neo.sh deploy-mta --host $1 --account $2 --source ./ui5app.mtar --synchronous --user $3 --password $4
+
+npm install ui5-nwabap-deployer-cli
+
+ui5-deployer --help
