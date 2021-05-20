1c5877c3b43daa12872c0b6945c776a9f7d6295b
Vivek Gowda <TheVivekGowda@gmail.com>
2021-05-20 19:21:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ mbt build --mtar -p=neo -t ui5app.mtar
 neo.sh deploy-mta --host $1 --account $2 --source ./ui5app.mtar --synchronous --user $3 --password $4
 
 # deploy app as BSP
-ui5-deployer deploy --server https://integrtrdev.integrtr.com:8443/ --client '100' --user $3 --pwd $4 --package ZUI5_DEPLOY --bspContainer ZUI5BP1 --bspContainerText 'Boilerplate' --transportNo INDK901302 --transportText 'UI5' --calculateApplicationIndex true
+ui5-deployer deploy --server https://integrtrdev.integrtr.com:8443/ --client '100' --user $3 --pwd $4 --package ZUI5_DEPLOY --bspContainer ZUI5BP --bspContainerText 'Boilerplate' --transportNo INDK901302 --transportText 'UI5' --calculateApplicationIndex true
