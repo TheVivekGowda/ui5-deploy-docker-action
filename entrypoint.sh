a41c81b3bb941e7bc8df14d27b8a721bc5c1c67b
Vivek Gowda <TheVivekGowda@gmail.com>
2021-05-20 22:27:46 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,8 @@ then
     neo.sh deploy-mta --host $4 --account $5 --source ./ui5app.mtar --synchronous --user $2 --password $3
 elif [ $1 == 'bsp' ]
 then
-    echo $3
     # deploy app as BSP
-    ui5-deployer deploy --user $1 --pwd $2
+    ui5-deployer deploy --server $4 --user $2 --pwd $3
 else
    echo "Please provide valid deployment type. Allowed values are neo and bsp"
 fi
