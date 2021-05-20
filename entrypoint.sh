bdd35c4b563f81136d53781b15b01070f5213fca
TheVivekGowda <TheVivekGowda@gmail.com>
2021-05-20 21:45:53 +0530

    fix: variable typo fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
-if [ $a == 'neo' ]
+if [ $1 == 'neo' ]
 then
     # build mtar file
     mbt build --mtar -p=neo -t ui5app.mtar
     # deploy mtar file to cloud
     neo.sh deploy-mta --host $3 --account $4 --source ./ui5app.mtar --synchronous --user $1 --password $2
-elif [ $a == 'bsp' ]
+elif [ $1 == 'bsp' ]
 then
     # deploy app as BSP
     ui5-deployer deploy --server $3 --user $1 --pwd $2
